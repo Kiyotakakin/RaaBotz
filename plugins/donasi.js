9fea54ba0f23017018427e92137311890ac8a56f
Kiyotakakin <115851952+Kiyotakakin@users.noreply.github.com>
2022-10-18 16:42:21 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,11 +1,10 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Tri [0895604670507]
-│ • Gopay  [0895604670507]
-│ • Dana  [0895604670507]
+│ • Axis [083172999950]
+│ • Dana  [083172999950]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/62895604670507
+│ > Ingin donasi? Wa.me/6283172999950
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
