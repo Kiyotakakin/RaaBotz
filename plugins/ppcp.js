199c2b031127f3a581db863291602d4e9869ebe2
Kiyotakakin <115851952+Kiyotakakin@users.noreply.github.com>
2022-10-25 00:27:32 +0700

    Update ppcp.js
    Signed-off-by: Kiyotakakin <115851952+Kiyotakakin@users.noreply.github.com>

--- plugins/ppcp.js
+++ plugins/ppcp.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn }) => {
 //https://api.lolhuman.xyz/api/random/ppcouple?apikey=RFK-Rey
 m.reply(wait)
 
-let res = await fetch(`https://api.xteam.xyz/randomimage/ppcouple?APIKEY=NezukoTachibana281207`)
+let res = await fetch(global.API('lol', `/api/random/ppcouple?`, {}, 'apikey'))
 let json = await res.json()
 
 conn.sendFile(m.chat, json.result.male, 'ppcowo.png', 'Cowok', m, false,{ contextInfo: { forwardingScore: 999, isForwarded: true }})
