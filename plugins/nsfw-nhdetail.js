b6158226f5fc73a86b943d946cd8fb3f0ca71e01
Kiyotakakin <115851952+Kiyotakakin@users.noreply.github.com>
2022-10-27 20:50:25 +0700

    Update nsfw-nhdetail.js

--- plugins/nsfw-nhdetail.js
+++ plugins/nsfw-nhdetail.js
@@ -1,24 +1,21 @@
 let fetch = require('node-fetch')
 let handler = async(m, { conn, args, usedPrefix, command}) => {
     if (!args[0]) throw `Masukkan kode, berikut: ${usedPrefix + command} 304307`
-    let res = await fetch(global.API('lol', `/api/nhentai/${args[0]}`, {}, 'apikey'))
+    let res = await fetch(`https://mxmxk.herokuapp.com/nhentai?code=${args[0]}`)
     if (!res.ok) throw await res.text()
     let json = await res.json()
     let ayaka = `
-Doujinshi Detail
-Title: ${json.result.title_romanji}
-Title (Native): ${json.result.title_native}
-Parodies: ${json.result.info.parodies}
-Characters: ${json.result.info.characters}
-Genres: ${json.result.info.tags}
-Artists: ${json.result.info.artists}
-Groups: ${json.result.info.groups}
-Categories: ${json.result.info.categories}
-Pages: ${json.result.info.pages}
-Uploaded: ${json.result.info.uploaded}
-Button tidak work untuk pesan sementara/wa mod silahkan pakai ${usedPrefix}nhpdf ${args[0]}
+𝙉𝙝𝙚𝙣𝙩𝙖𝙞 𝘿𝙚𝙩𝙖𝙞𝙡:
+*Id:* _${json.result.id}_
+*Title:* _${json.result.title.japanese}_
+*Title (Native):* _${json.result.title.english}_
+*language:* _${json.result.lang}_
+*Pages:* _${json.result.num_pages}_
+*favorite:* _${json.result.num_favorites}_
+*Uploaded:* _${json.result.upload_time}_
+*_Button tidak work untuk pesan sementara/wa mod silahkan pakai ${usedPrefix}nhpdf ${args[0]}_*
 `.trim()
-    conn.sendButtonImg(m.chat, await (await fetch(json.result.image[0])).buffer(), ayaka, wm, 'Download', `.nhd ${args[0]}`, m)
+    conn.sendButtonImg(m.chat, await (await fetch(json.result.images.cover)).buffer(), ayaka, packname, 'Download', `.nhpdf ${args[0]}`, m)
 }
 handler.command = /^nh$/i
 handler.tags = ['nsfw','internet']
